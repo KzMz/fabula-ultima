a9f87752c4a070b8752f3339d5a2704242983e14
Mattia Bruno <mattia.bruno.kzmz@gmail.com>
2021-07-28 23:33:43 +0200

    boilerplate

---
+++ module/helpers/config.mjs
@@ -0,0 +1,29 @@
+export const FABULAULTIMA = {};
+
+// Define constants here, such as:
+FABULAULTIMA.foobar = {
+  'bas': 'FABULAULTIMA.bas',
+  'bar': 'FABULAULTIMA.bar'
+};
+
+/**
+ * The set of Ability Scores used within the sytem.
+ * @type {Object}
+ */
+ FABULAULTIMA.abilities = {
+  "str": "FABULAULTIMA.AbilityStr",
+  "dex": "FABULAULTIMA.AbilityDex",
+  "con": "FABULAULTIMA.AbilityCon",
+  "int": "FABULAULTIMA.AbilityInt",
+  "wis": "FABULAULTIMA.AbilityWis",
+  "cha": "FABULAULTIMA.AbilityCha"
+};
+
+FABULAULTIMA.abilityAbbreviations = {
+  "str": "FABULAULTIMA.AbilityStrAbbr",
+  "dex": "FABULAULTIMA.AbilityDexAbbr",
+  "con": "FABULAULTIMA.AbilityConAbbr",
+  "int": "FABULAULTIMA.AbilityIntAbbr",
+  "wis": "FABULAULTIMA.AbilityWisAbbr",
+  "cha": "FABULAULTIMA.AbilityChaAbbr"
+};
---
+++ module/helpers/templates.mjs
@@ -0,0 +1,15 @@
+/**
+ * Define a set of template paths to pre-load
+ * Pre-loaded templates are compiled and cached for fast access when rendering
+ * @return {Promise}
+ */
+ export const preloadHandlebarsTemplates = async function() {
+  return loadTemplates([
+
+    // Actor partials.
+    "systems/fabulaultima/templates/actor/parts/actor-features.html",
+    "systems/fabulaultima/templates/actor/parts/actor-items.html",
+    "systems/fabulaultima/templates/actor/parts/actor-spells.html",
+    "systems/fabulaultima/templates/actor/parts/actor-effects.html",
+  ]);
+};
