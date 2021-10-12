fa72b9d1a1f72eb394c138b30aaa981f1584c064
Mattia Bruno <mattia.bruno.kzmz@gmail.com>
2021-10-12 02:26:35 +0200

    battle ui test

--- module/fabulaultima.mjs
+++ module/fabulaultima.mjs
@@ -74,6 +74,9 @@ Handlebars.registerHelper('toLowerCase', function(str) {
 Hooks.once("ready", async function() {
   // Wait to register hotbar drop hook on ready so that modules could register earlier if they want to
   Hooks.on("hotbarDrop", (bar, data, slot) => createItemMacro(data, slot));
+
+  const battleHud = $('<div id="battle-hud"></div>');
+  $("#" + game.canvas.hud.id).appendChild(battleHud);
 });
 
 /* -------------------------------------------- */
